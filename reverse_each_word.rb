061a3b37108298851bec7fad74c3c1635b238601
Omar Nabi <onabi20@gmail.com>
2020-03-31 04:46:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,2 @@
-def reverse_each_word_with_each(string)
+def reverse_each_word(sentence) reversed = sentence.split(" ").collect do |word| word.reverse end reversed.join(" ") end
   
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
-end
